a1cc068615945af58407160e11ebd92562a9168f
Qu1ck_1337 <Glebkraykin@yandex.ru>
2021-12-26 13:22:27 +0300

    new roles in roulette

--- luckerRole.py
+++ luckerRole.py
@@ -11,7 +11,7 @@ class LuckerRole(commands.Cog):
     @commands.command(name="spin", aliases=["спин", "крутить", "spins"])
     async def spin(self, ctx):
         if ctx.channel.id == luckyRoleSettings["luckyRoleChannelID"]:
-            await ctx.reply(random.choice(luckyRoleSettings["luckyRolePhraze"]))
+            # await ctx.reply(random.choice(luckyRoleSettings["luckyRolePhraze"]))
             randomNumber = random.randint(0, luckyRoleSettings["luckyRoleMax"])
             if 777 == randomNumber:
                 role = ctx.guild.get_role(luckyRoleSettings["luckyRoleID"])
@@ -23,11 +23,31 @@ class LuckerRole(commands.Cog):
                                                   f"\nПолучай себе заветную роль: **{role.name}**! Теперь можешь перед всеми понтоваться) 😎",
                                       colour=0xffd700
                                       )
+            elif 666 == randomNumber:
+                role = ctx.guild.get_role(luckyRoleSettings["DemonRoleID"])
+                await ctx.author.add_roles(role)
+                embed = discord.Embed(title=random.choice(luckyRoleSettings["luckyRoleWinPhraze"]),
+                                      description=f"**Выпало число:**"
+                                                  f"\n\n👹 **`{randomNumber}`** 👹"
+                                                  f"\n\n**Удача на твоей стороне!** 💎"
+                                                  f"\nПолучай себе заветную роль: **{role.name}**! Теперь можешь перед всеми понтоваться) 😎",
+                                      colour=0xffd700
+                                      )
+            elif 1 == randomNumber:
+                role = ctx.guild.get_role(luckyRoleSettings["FirstRoleID"])
+                await ctx.author.add_roles(role)
+                embed = discord.Embed(title=random.choice(luckyRoleSettings["luckyRoleWinPhraze"]),
+                                      description=f"**Выпало число:**"
+                                                  f"\n\n🕶️ **`{randomNumber}`** 🕶️"
+                                                  f"\n\n**Удача на твоей стороне!** 💎"
+                                                  f"\nПолучай себе заветную роль: **{role.name}**! Теперь можешь перед всеми понтоваться) 😎",
+                                      colour=0xffd700
+                                      )
             else:
                 embed = discord.Embed(title="В следующий раз повезёт)",
                                       description=f"**Выпало число:**"
                                                   f"\n\n❌ **`{randomNumber}`** ❌"
-                                                  f"\n\nКрути сколько хочешь, пока не выпадет число **777**! Может тебе когда-нибудь повезёт) 😏",
+                                                  f"\n\nСпины не ограничены. Может быть тебе когда-нибудь повезёт) 😏",
                                       colour=0xff0000
                                       )
 
