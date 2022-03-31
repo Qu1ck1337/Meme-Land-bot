9571d7335e468b93f0dacd5e67e9199db56fbd1c
Qu1ck1337 <Glebkraykin@yandex.ru>
2022-03-31 21:17:17 +0300

    application id was changed to master branch bot

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ import asyncio
 import datetime
 import os
 import discord
-from discord import app_commands
 from discord.ext import commands, tasks
 from config import settings, beta_settings
 
 intents = discord.Intents(guilds=True, members=True, emojis=True, presences=True, messages=True, reactions=True, typing=True)
 
 bot = commands.Bot(command_prefix=settings['prefix'], help_command=None, intents=intents,
-                   application_id=934900322634190878)
+                   application_id=894952935442747393)
 
 @bot.event
 async def on_ready():
